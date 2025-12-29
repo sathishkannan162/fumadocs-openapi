f73d80f350e614e05f2b18158d3b640a38ce50b0
Sathish Kannan <sathishkannan162@gmail.com>
2025-12-29 23:39:00 +0530

    Fix links not present

--- lib/openapi.ts
+++ lib/openapi.ts
@@ -2,5 +2,5 @@ import { createOpenAPI } from 'fumadocs-openapi/server';
 
 export const openapi = createOpenAPI({
   // the OpenAPI schema, you can also give it an external URL.
-  input: ['./petstore-openapi.json'],
+  input: ['./train-openapi.json'],
 });
