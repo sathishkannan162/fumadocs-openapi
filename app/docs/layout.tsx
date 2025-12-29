b19e20ef4e632ea14c717869829e8d4ccaf0ff5f
Sathish Kannan <sathishkannan162@gmail.com>
2025-12-30 02:04:11 +0530

    Add ai chat functionality

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,12 @@
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
+import { AISearchPanel } from '@/components/aiSearch';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions()}>
+      <AISearchPanel />
       {children}
     </DocsLayout>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,28 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
-import SearchDialog from '@/components/search';
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "./global.css";
+import { Inter } from "next/font/google";
+import { AISearch, AISearchPanel, AISearchTrigger } from "@/components/aiSearch";
+import SearchDialog from "@/components/search";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <AISearch>
+          <AISearchTrigger />
         <RootProvider
           search={{
             SearchDialog,
           }}
-        >{children}</RootProvider>
+        >
+          {children}
+        </RootProvider>
+
+        </AISearch>
       </body>
     </html>
   );
