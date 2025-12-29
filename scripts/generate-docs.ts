860d0db442941c929265ca90559958fdfb6674fc
Sathish Kannan <sathishkannan162@gmail.com>
2025-12-30 00:59:16 +0530

    Add index page and group by tag

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -4,7 +4,23 @@ import { openapi } from '../lib/openapi.ts';
 void generateFiles({
   input: openapi,
   output: './content/docs',
+ index: {
+    // for generating `href`
+    url: {
+      baseUrl: '/docs',
+      contentDir: './content/docs',
+    },
+    items: [
+      {
+        path: 'index.mdx',
+        // optional: set frontmatter
+        description: 'All available pages',
+      },
+    ],
+  },
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
+  per: 'operation',
+  groupBy: 'tag',
 });
