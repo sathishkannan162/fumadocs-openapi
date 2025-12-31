f212540f0778726d774cff3b1c94452a31893a83
Sathish Kannan <sathishkannan162@gmail.com>
2025-12-31 17:31:28 +0530

    Set full: true for index page

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,26 +1,26 @@
-import { generateFiles } from 'fumadocs-openapi';
-import { openapi } from '../lib/openapi.ts';
+import { generateFiles } from "fumadocs-openapi";
+import { openapi } from "../lib/openapi.ts";
 
 void generateFiles({
   input: openapi,
-  output: './content/docs',
- index: {
+  output: "./content/docs",
+  index: {
     // for generating `href`
     url: {
-      baseUrl: '/docs',
-      contentDir: './content/docs',
+      baseUrl: "/docs",
+      contentDir: "./content/docs",
     },
     items: [
       {
-        path: 'index.mdx',
+        path: "index.mdx",
         // optional: set frontmatter
-        description: 'All available pages',
+        description: "All available pages",
       },
     ],
   },
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
-  per: 'operation',
-  groupBy: 'tag',
+  per: "operation",
+  groupBy: "tag",
 });
